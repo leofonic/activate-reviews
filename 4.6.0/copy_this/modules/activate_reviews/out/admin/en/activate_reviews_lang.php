24890937f2b143a8649aa167ef6f8395ec31a1a7
Frank Zunderer <info@zunderweb.de>
2012-09-26 22:47:07 +0200

    Initial Commit

---
+++ 4.6.0/copy_this/modules/activate_reviews/out/admin/en/activate_reviews_lang.php
@@ -0,0 +1,12 @@
+<?php
+$sLangName  = "Deutsch";
+$iLangNr    = 0;
+$aLang = array(
+
+'charset'                                   => 'ISO-8859-15',
+'z_mxremlist'                               => 'Activate Reviews',
+'AR_DASHBOARD_MESSAGE_1'                    => 'There are ',
+'AR_DASHBOARD_MESSAGE_2'                    => ' new reviews to activate.',
+'AR_NO_MODERATING'                          => 'Moderation of Reviews not activated!',
+
+);
