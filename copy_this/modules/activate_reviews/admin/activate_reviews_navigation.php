1fbdb22340927727bff07bd6c6f14540946e3304
Frank Zunderer <info@zunderweb.de>
2018-10-20 02:57:54 +0200

    Update copy_this/modules/activate_reviews/admin/activate_reviews_navigation.php

--- copy_this/modules/activate_reviews/admin/activate_reviews_navigation.php
+++ copy_this/modules/activate_reviews/admin/activate_reviews_navigation.php
@@ -1,21 +1,28 @@
 <?php
-  class activate_reviews_navigation extends activate_reviews_navigation_parent {
- 
-	  protected function _doStartUpChecks()
-	 {	
-	 	$aMessage = array ();
-		$aMessage = parent::_doStartUpChecks();
-		$myConfig = $this->getConfig();
-		if ($myConfig->getConfigParam( 'blGBModerate' )){
-		$sSelect = "SELECT count(*) FROM `oxreviews` WHERE `OXACTIVE` = 0";
-		$rs = oxDb::getDb()->Getone($sSelect);
-			if ($rs > 0){
-			$aMessage['message'] = '<a href="?cl=z_review&amp;stoken='.$this->getSession()->getSessionChallengeToken().'" target="basefrm">'.
-            oxRegistry::getLang()->translateString('AR_DASHBOARD_MESSAGE_1')
-            .$rs.oxRegistry::getLang()->translateString('AR_DASHBOARD_MESSAGE_2').'</a>';
-			}
-		}
-	 	return $aMessage;
-	 }
- }
+class activate_reviews_navigation extends activate_reviews_navigation_parent {
+
+    protected function _doStartUpChecks()
+    {	
+        $aMessage = array ();
+        $aMessage = parent::_doStartUpChecks();
+        $myConfig = $this->getConfig();
+        if ($myConfig->getConfigParam( 'blGBModerate' )){
+            $sSelect = "SELECT count(*) FROM `oxreviews` WHERE `OXACTIVE` = 0";
+            $rs = oxDb::getDb()->Getone($sSelect);
+            if ($rs > 0){
+                if (!$aMessage['message']) $aMessage['message'] = '';
+                else $aMessage['message'] .= ' ';
+                
+                $aMessage['message'] .= '<a href="?cl=z_review&amp;stoken='
+                .$this->getSession()->getSessionChallengeToken()
+                .'" target="basefrm">'
+                .oxRegistry::getLang()->translateString('AR_DASHBOARD_MESSAGE_1')
+                .$rs
+                .oxRegistry::getLang()->translateString('AR_DASHBOARD_MESSAGE_2')
+                .'</a>';
+            }
+        }
+        return $aMessage;
+    }
+}
 ?>
